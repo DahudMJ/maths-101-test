447d41e792d40a37a452768b6ec1586adbdd947b
DahudMJ <108504988+DahudMJ@users.noreply.github.com>
2022-10-10 02:45:36 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "If sin a = 1/root3 and 0 </= a </= 90, find cosec a",
+    choice1: "root 3",
+    choice2: "root 2",
+    choice3: "root3/2",
+    choice4: "root3/root2",
+    answer: 1
+},
+            {
+            question: "Given that z1= 2+i, z2= 3-2i, z3= 5-4i, find the modulus of z2 and conjuguate of z1 respectively",
+    choice1: "root 2 & 2+i",
+    choice2: "root3/2 & 2-i",
+    choice3: "root15 & 2-i",
+    choice4: "root15 & 2+i",
+    answer: 2
+},
+            {
+            question: "Given f(t)=2t^2 and g(t)=3t + 1,  find f o g", 
+    choice1: "8",
+    choice2: "32",
+    choice3: "64",
+    choice4: "16",
+    answer: 1
+},
+            {
+            question: "a set that has a finite number of element is known as ? ",
+    choice1: "finite set",
+    choice2: "null set",
+    choice3: "empty set",
+    choice4: "infinite",
+    answer: 0
+},
+            {
+            question: "solve (3+2i)+(-7-i)",
+    choice1: "-4+i",
+    choice2: "-4-i",
+    choice3: "4+i",
+    choice4: "4-i",
+    answer: 1
+},
+            {
+            question: "(-1+3i)(5-7i)",
+    choice1: "16-22i",
+    choice2: "-16+22i",
+    choice3: "16+22i",
+    choice4: "-16-22i",
+    answer: 2
+},
+            {
+               question: "[Re[(5+3i)+(-1+2i)]]^2",
+    choice1: "25",
+    choice2: "9",
+    choice3: "36",
+    choice4: "16",
+    answer: 3
+},{
+               question: "U = {x:x E N, x < 25} if A= {number divisible by 2^2}, find A",
+    choice1: "{2,4,8,16,20,24}",
+    choice2: "{4,8,16,20,24}",
+    choice3: "{2,4,8,12,16,20,24}",
+    choice4: "{2,4,6,8,10,12,14,16,18,20,24}",
+    answer: 2
+},
+            {
+               question: "Tan 15 = ?",
+    choice1: "2/root3",
+    choice2: "1/root2",
+    choice3: "1/root3",
+    choice4: "root2/root3",
+    answer: 2
+},
+            {
+               question: "If a function f: a - b satisfies both one to one and onto functions properties, the function is said to be ?",
+    choice1: "Bijective",
+    choice2: "Duojective",
+    choice3: "Surjective",
+    choice4: "Injective",
+    answer: 0
+}];
